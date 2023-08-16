71bf1007de4c411deb261189bbb588eb060021b7
Mreldesign <120594811+Mreldesign@users.noreply.github.com>
2023-08-16 09:31:27 -0300

    test

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+const astroCards = document.querySelectorAll('.astro-card-information');
+const filterSelect = document.querySelector('.astro-options');
+const searchInput = document.querySelector('.input');
+
+filterSelect.addEventListener('change', () => {
+  const filterValue = filterSelect.value;
+  astroCards.forEach(card => {
+    if (filterValue === 'All' || card.dataset.type === filterValue) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+
+searchInput.addEventListener('input', () => {
+  const searchValue = searchInput.value.toLowerCase();
+  astroCards.forEach(card => {
+    const cardName = card.querySelector('.astro-card-name').textContent.toLowerCase();
+    if (cardName.includes(searchValue)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+
